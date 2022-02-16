46ecf9230123c79b845e07900424ddaa1450f2f4
Cheryl <cherylsychua@outlook.com>
2022-02-16 18:01:35 +0000

    chore: standardise indentation to 2 spaces

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get('/healthcheck' , (req, res) => {
-    res.send('All good!')
-})
+app.get('/healthcheck', (req, res) => {
+  res.send('All good!');
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
+hjgjh;
